5f9c3895fb6f20f125b22fb6cea9c95aabc82c2a
Quynh Anh <annabel.anh@gmail.com>
2024-04-19 14:36:41 -0600

    remove console.log

--- src/views/TeamPage/EditTeam.jsx
+++ src/views/TeamPage/EditTeam.jsx
@@ -42,7 +42,6 @@ export default function EditTeam({isCreate}) {
 
     const onSubmit = async (data) => {
         data = {...data}
-        console.log(data)
         isCreate ? await api.create(data) : await api.update(data)
         navigate('/teams')
     }
